c993efbf27dfdd464652a1b24368e4e04af346cc
Balance8 <mslocum819@gmail.com>
2021-09-29 10:20:54 -0700

    initial commit

---
+++ apps/epsilon3-web-app/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'epsilon3-web-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/epsilon3-web-app',
+};
