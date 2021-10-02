178568e7df5797c2330df3317ceea9f92f8d6ddd
Balance8 <mslocum819@gmail.com>
2021-10-02 14:26:23 -0700

    repair mobile light switch

--- libs/shared/ui/src/lib/header/header.tsx
+++ libs/shared/ui/src/lib/header/header.tsx
@@ -229,39 +229,45 @@ export function Header(props: HeaderProps) {
           <div className="inline-flex md:hidden">
             {/* Mobile lights switch */}
             <div className="form-switch focus-within:outline-blue flex flex-col justify-center mr-6 -mt-0.5">
-              <input
-                type="checkbox"
-                name="light-switch"
-                id="light-switch-mobile"
-                className="sr-only light-switch"
-                checked={darkMode}
-                onChange={() => setDarkMode(!darkMode)}
-              />
-              <label className="relative" htmlFor="light-switch-mobile">
-                <span
-                  className="relative z-10 shadow-sm bg-gradient-to-t from-gray-100 to-white dark:from-gray-800 dark:to-gray-700"
-                  aria-hidden="true"
-                ></span>
-                <svg
-                  className="absolute inset-0"
-                  width="44"
-                  height="24"
-                  viewBox="0 0 44 24"
-                  xmlns="http://www.w3.org/2000/svg"
+              {typeof window !== 'undefined' && (
+                <Switch
+                  checked={darkMode}
+                  onChange={() => setDarkMode(!darkMode)}
+                  className={classNames(
+                    darkMode ? 'bg-teal-500' : 'bg-teal-500',
+                    'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                  )}
                 >
-                  <g
-                    className="text-white fill-current"
-                    fillRule="nonzero"
-                    opacity=".88"
+                  <span className="sr-only">Use setting</span>
+                  <span
+                    className={classNames(
+                      darkMode
+                        ? 'translate-x-5 bg-gray-800 '
+                        : 'translate-x-0 bg-white',
+                      'pointer-events-none relative inline-block h-5 w-5 rounded-full  shadow transform ring-0 transition ease-in-out duration-200'
+                    )}
                   >
-                    <path d="M32 8a.5.5 0 00.5-.5v-1a.5.5 0 10-1 0v1a.5.5 0 00.5.5zM35.182 9.318a.5.5 0 00.354-.147l.707-.707a.5.5 0 00-.707-.707l-.707.707a.5.5 0 00.353.854zM37.5 11.5h-1a.5.5 0 100 1h1a.5.5 0 100-1zM35.536 14.829a.5.5 0 00-.707.707l.707.707a.5.5 0 00.707-.707l-.707-.707zM32 16a.5.5 0 00-.5.5v1a.5.5 0 101 0v-1a.5.5 0 00-.5-.5zM28.464 14.829l-.707.707a.5.5 0 00.707.707l.707-.707a.5.5 0 00-.707-.707zM28 12a.5.5 0 00-.5-.5h-1a.5.5 0 100 1h1a.5.5 0 00.5-.5zM28.464 9.171a.5.5 0 00.707-.707l-.707-.707a.5.5 0 00-.707.707l.707.707z" />
-                    <circle cx="32" cy="12" r="3" />
-                    <circle fillOpacity=".4" cx="12" cy="12" r="6" />
-                    <circle fillOpacity=".88" cx="12" cy="12" r="3" />
-                  </g>
-                </svg>
-                <span className="sr-only">Switch to light / dark version</span>
-              </label>
+                    <span
+                      className={classNames(
+                        darkMode
+                          ? 'opacity-0 ease-out duration-100'
+                          : 'opacity-100 ease-in duration-200',
+                        'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity'
+                      )}
+                      aria-hidden="true"
+                    ></span>
+                    <span
+                      className={classNames(
+                        darkMode
+                          ? 'opacity-100 ease-in duration-200'
+                          : 'opacity-0 ease-out duration-100',
+                        'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity'
+                      )}
+                      aria-hidden="true"
+                    ></span>
+                  </span>
+                </Switch>
+              )}
             </div>
 
             {/* Hamburger button */}
